f154e02664407415a95fca48246fdd7f4a0683b5
rajpootjyoti700 <133164017+rajpootjyoti700@users.noreply.github.com>
2023-10-22 13:30:11 +0530

    Time: 1 ms (99.96%), Space: 44 MB (28.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,24 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> answer = new ArrayList<>();
-        // Arrays.sort(candidates);
-        sum(answer, new ArrayList<>(), candidates, target, candidates.length - 1);
-        return answer;
+        public void sum(int index,int[] arr, int target,List<List<Integer>> ans,List<Integer> list){
+        if(index==arr.length){
+            if(target==0){
+                ans.add(new ArrayList<>(list));
+            }
+            return;
+        }
+         if(arr[index]<=target){
+             list.add(arr[index]);
+             sum(index,arr,target-arr[index],ans,list);
+             list.remove(list.size()-1);
+         }
+            
+         sum(index+1,arr,target,ans,list);
     }
 
-    public void sum(List<List<Integer>> answer, List<Integer> list, int[] candidates, int target, int index) {
-        if (index == -1) {
-            if (target == 0)
-                answer.add(new ArrayList<>(list));
-            return;
-        }
-        if (target < 0)
-            return;
-        if (target == 0) {
-            answer.add(new ArrayList<>(list));
-            return;
-        }
-        list.add(candidates[index]);
-        sum(answer, list, candidates, target - candidates[index], index);
-        list.remove(list.size() - 1);
-        sum(answer, list, candidates, target, index - 1); 
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        sum(0,candidates,target,ans,list);
+        return ans;
     }
 }
