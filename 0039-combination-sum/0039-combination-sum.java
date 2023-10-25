5cffe48ba806e5636dc03ee435447336174a26e6
rajpootjyoti700 <133164017+rajpootjyoti700@users.noreply.github.com>
2023-10-25 12:16:13 +0530

    Time: 1 ms (99.96%), Space: 43.9 MB (38.81%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,24 @@
 class Solution {
-        public void sum(int index,int[] arr, int target,List<List<Integer>> ans,List<Integer> list){
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        sum(0,candidates,target,ans,list);
+        return ans;
+        
+    }
+    private void sum(int index, int[] arr, int target,List<List<Integer>> ans,List<Integer> list ){
         if(index==arr.length){
             if(target==0){
                 ans.add(new ArrayList<>(list));
             }
             return;
         }
-         if(arr[index]<=target){
-             list.add(arr[index]);
-             sum(index,arr,target-arr[index],ans,list);
-             list.remove(list.size()-1);
-         }
+        if(arr[index]<=target){
+            list.add(arr[index]);
+            sum(index,arr,target-arr[index],ans,list);
+            list.remove(list.size()-1);
             
-         sum(index+1,arr,target,ans,list);
-    }
-
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> list=new ArrayList<>();
-        sum(0,candidates,target,ans,list);
-        return ans;
+        }
+        sum(index+1,arr,target,ans,list);
     }
 }
