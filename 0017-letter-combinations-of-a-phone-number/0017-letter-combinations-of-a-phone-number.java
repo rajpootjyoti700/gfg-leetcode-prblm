5bc1991db9f29894691407ed387a3136705120f1
rajpootjyoti700 <133164017+rajpootjyoti700@users.noreply.github.com>
2023-10-26 23:49:50 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (90.81%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,25 +1,27 @@
 class Solution {
-   public  void phn(int index,StringBuilder sb,String[] map,List<String> ans,String digits){
+    public void phn(int index,String digits,StringBuilder sb,List<String> ans,String[] map){
         if(index>=digits.length()){
-            ans.add(new String(sb));
-            return;
+            ans.add(new String(sb));  // convert the string builder into string
+                return;
         }
         int number=digits.charAt(index)-'0';
         String value=map[number];
         for(int i=0;i<value.length();i++){
             sb=sb.append(value.charAt(i));
-            phn(index+1,sb,map,ans,digits);
+            phn(index+1,digits,sb,ans,map);
             sb.deleteCharAt(sb.length()-1);
         }
+            
     }
     public List<String> letterCombinations(String digits) {
-        ArrayList<String> ans=new ArrayList<String>();
+        List<String> ans=new ArrayList<>();
         if(digits.length()==0)
             return ans;
+            
         int index=0;
-        StringBuilder sb=new StringBuilder("");
         String[] map={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        phn(index,sb,map,ans,digits);
+        StringBuilder sb=new StringBuilder("");
+        phn(index,digits,sb,ans,map);
         return ans;
         
     }
