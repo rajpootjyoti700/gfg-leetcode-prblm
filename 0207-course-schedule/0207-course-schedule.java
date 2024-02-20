cff980354d9695c66fd191f108f574ce3380f7c2
rajpootjyoti700 <133164017+rajpootjyoti700@users.noreply.github.com>
2024-02-20 17:08:07 +0530

    Time: 5 ms (72.35%), Space: 45 MB (50.32%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        int n=numCourses;
+        int m=prerequisites.length;
+        int[] indeg=new int[n];
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<m;i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        for(int i=0;i<n;i++){
+            for(int it:adj.get(i)){
+                indeg[it]++;
+            }
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0)
+                q.add(i);    
+        }
+        int[] topo=new int[n];
+        int indx=0;
+        while(!q.isEmpty()){
+            int node=q.peek();
+                topo[indx]=node;
+            indx++;
+            q.remove();
+            for(int it: adj.get(node)){
+                indeg[it]--;
+                if(indeg[it]==0)
+                    q.add(it);
+            }
+        }
+        if(indx==n)
+            return true;
+       // int[] arr={};
+        return false;
+    }
+}
