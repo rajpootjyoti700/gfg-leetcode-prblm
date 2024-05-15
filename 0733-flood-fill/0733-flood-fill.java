7c5f39c1a7be61102a4be29fefa9e5d3af385af7
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-05-15 08:46:01 +0530

    Time: 1 ms (39.81%), Space: 44.7 MB (74.16%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,23 +1,35 @@
 class Solution {
-    public void dfs(int row,int col,int[][] ans,int[][] image,int color,int incolor,int[] drow,int[] dcol){
-        ans[row][col]=color;
-        int n=image.length;
-        int m=image[0].length;
-        for(int i=0;i<4;i++){
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            if(nrow>=0 && nrow<n &&ncol>=0 && ncol<m && image[nrow][ncol]==incolor && ans[nrow][ncol]!=color){
-                
-    dfs(nrow,ncol,ans,image,color,incolor,drow,dcol);
+    
+    private void dfs(int row, int col, 
+     int[][] ans,
+     int[][] image, 
+     int color, int delRow[], int delCol[],
+     int iniColor) {
+        // color with new color
+        ans[row][col] = color; 
+        int n = image.length;
+        int m = image[0].length; 
+        // there are exactly 4 neighbours
+        for(int i = 0;i<4;i++) {
+            int nrow = row + delRow[i]; 
+            int ncol = col + delCol[i]; 
+            // check for valid coordinate 
+            // then check for same initial color and unvisited pixel
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && 
+            image[nrow][ncol] == iniColor && ans[nrow][ncol] != color) {
+                dfs(nrow, ncol, ans, image, color, delRow, delCol, iniColor); 
             }
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int incolor=image[sr][sc];
-        int[][] ans=image;
-        int[] drow={-1,0,+1,0};
-        int[] dcol={0,1,0,-1};
-        dfs(sr,sc,ans,image,color,incolor,drow,dcol);
-        return ans;
+        // get initial color
+        int iniColor = image[sr][sc]; 
+        int[][] ans = image; 
+        // delta row and delta column for neighbours
+        int delRow[] = {-1, 0, +1, 0};
+        int delCol[] = {0, +1, 0, -1}; 
+        dfs(sr, sc, ans, image, color, delRow, delCol, iniColor); 
+        return ans;  
+    
     }
 }
