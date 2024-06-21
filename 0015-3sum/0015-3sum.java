abbd04f26a7e751431721769acfe3093ce852cd6
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-06-21 17:57:03 +0530

    Time: 840 ms (16.09%), Space: 52.2 MB (29.26%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,48 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Sort the array
+        // if we apply brute then the complexity will n^3 which is very high
+        // we can apply two pointer approch on this
+        if(nums==null || nums.length<3)
+            return new ArrayList<>();
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    // Found a triplet with zero sum
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicate elements for j
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-
-                    // Skip duplicate elements for k
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    // Move the pointers
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    // Sum is less than zero, increment j to increase the sum
-                    j++;
-                } else {
-                    // Sum is greater than zero, decrement k to decrease the sum
-                    k--;
-                }
-            }
+        HashSet<List<Integer>> result=new HashSet<>();
+        int n=nums.length;
+        for(int i=0;i<n-2;i++){
+            int left=i+1;
+            int right=nums.length-1;
+             while(left<right){
+                 int sum=nums[i]+nums[left]+nums[right];
+                 if(sum==0){
+                     result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                     left++;
+                     right--;
+                 }
+                 else if(sum<0)
+                     left++;
+                 else
+                     right--;
+             }
         }
-        return ans;
+        return new ArrayList<>(result);
     }
 }
