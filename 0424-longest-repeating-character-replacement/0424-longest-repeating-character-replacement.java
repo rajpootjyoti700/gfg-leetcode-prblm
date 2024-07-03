79107678adbf224c590961abb466436997cb9023
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-07-03 18:50:18 +0530

    Time: 11 ms (48.93%), Space: 42.5 MB (48.28%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,30 +1,20 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-      // int[] arr = new int[26];
-      //  int largestCount = 0, start = 0, maxlen = 0;
-       // for(int end = 0; end < s.length(); end ++){
-          //  arr[s.charAt(end) - 'A']++;
-           // largestCount = Math.max(largestCount, arr[s.charAt(end) - 'A']);
-           // if(end - start + 1 - largestCount > k){ 
-            //    arr[s.charAt(start) - 'A']--;
-            ////    start ++;
-          //  }
-          //  maxlen = Math.max(maxlen, end - start + 1);
-       // }
-       // return maxlen; 
-        
-        // so the approach is taking a 26 size array  which contain frequency of each character
-        //we have to find out the maxlength-  maxfrequncy if this diff is grater than k then break. becoz this diff is the no of changes requiered.
+    // the main concept is max window leength -max frequency if this diffrence 
+        // exceed the value of k means that we cant convert so we have shrink our window 
+        // from the left
+        // again keep track the max length;
         int[] arr=new int[26];
-        int maxf=0;
         int maxlen=0;
+        int maxf=0;
         int l=0;
         int r=0;
         for( r=0;r<s.length();r++){
+            // store the freq in the this array
             arr[s.charAt(r)-'A']++;
             maxf=Math.max(maxf,arr[s.charAt(r)-'A']);
-            
             if(r-l+1-maxf>k){
+                // shrink grom the left
                 arr[s.charAt(l)-'A']--;
                 l++;
             }
