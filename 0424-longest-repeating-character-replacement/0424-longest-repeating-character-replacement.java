814628be4aa2199cbdcbb128118e46e0425205d2
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-07-03 18:42:04 +0530

    Time: 10 ms (62.63%), Space: 42.4 MB (67.71%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,16 +1,35 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-       int[] arr = new int[26];
-        int largestCount = 0, start = 0, maxlen = 0;
-        for(int end = 0; end < s.length(); end ++){
-            arr[s.charAt(end) - 'A']++;
-            largestCount = Math.max(largestCount, arr[s.charAt(end) - 'A']);
-            if(end - start + 1 - largestCount > k){ 
-                arr[s.charAt(start) - 'A']--;
-                start ++;
+      // int[] arr = new int[26];
+      //  int largestCount = 0, start = 0, maxlen = 0;
+       // for(int end = 0; end < s.length(); end ++){
+          //  arr[s.charAt(end) - 'A']++;
+           // largestCount = Math.max(largestCount, arr[s.charAt(end) - 'A']);
+           // if(end - start + 1 - largestCount > k){ 
+            //    arr[s.charAt(start) - 'A']--;
+            ////    start ++;
+          //  }
+          //  maxlen = Math.max(maxlen, end - start + 1);
+       // }
+       // return maxlen; 
+        
+        // so the approach is taking a 26 size array  which contain frequency of each character
+        //we have to find out the maxlength-  maxfrequncy if this diff is grater than k then break. becoz this diff is the no of changes requiered.
+        int[] arr=new int[26];
+        int maxf=0;
+        int maxlen=0;
+        int l=0;
+        int r=0;
+        for( r=0;r<s.length();r++){
+            arr[s.charAt(r)-'A']++;
+            maxf=Math.max(maxf,arr[s.charAt(r)-'A']);
+            
+            if(r-l+1-maxf>k){
+                arr[s.charAt(l)-'A']--;
+                l++;
             }
-            maxlen = Math.max(maxlen, end - start + 1);
+            maxlen=Math.max(maxlen,r-l+1);
         }
-        return maxlen; 
+        return maxlen;
     }
 }
