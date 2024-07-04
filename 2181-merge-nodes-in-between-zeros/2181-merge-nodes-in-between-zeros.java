83ad181c7c7221a33b8b0ea2731083294c27415d
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-07-04 10:19:48 +0530

    Time: 6 ms (31.54%), Space: 82.2 MB (41.19%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -10,19 +10,24 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
+        // thelogic is we will traverse the whole link and when we got 
+        // the 0 create a new node nd store the sum value
+        //and before that we have to crate the dummy node so that the next dummy will be our 
+        // 1st sum nd next to next
         int sum=0;
-        ListNode temp=head;
         ListNode temp1=new ListNode();
-       ListNode cur=temp1;
-        
+        ListNode cur=temp1;
+        ListNode temp=head;
         while(temp.next!=null){
             sum=sum+temp.val;
             temp=temp.next;
             if(temp.val==0){
-            cur.next=new ListNode(sum);
+            
+                cur.next=new ListNode(sum);
                 cur=cur.next;
-                sum=0;   
+                sum=0;
             }
+            
         }
         return temp1.next;
     }
