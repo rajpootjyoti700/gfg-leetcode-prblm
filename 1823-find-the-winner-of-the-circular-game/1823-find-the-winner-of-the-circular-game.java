dc150c6122597cd8f686363f4fe17b365e2fe0b5
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-07-08 10:50:37 +0530

    Time: 0 ms (100.00%), Space: 41 MB (29.98%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,11 +1,13 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-         return helper(n,k)+1;
-}
-public int helper(int n,int k){
-    if(n==1)
-        return 0;
-    else
-        return (helper(n-1,k)+k)%n;
+        // the aapproch is as it is in circular manner so modulo are needed
+        // and we have to remove after every kth position 
+        // ans +k the the position we want to eliminate 
+        // and modulo of i are just becoz circle;
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans=(ans+k)%i;
+        }
+        return ans+1;
     }
 }
