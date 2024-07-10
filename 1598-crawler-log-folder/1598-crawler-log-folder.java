8e2cf90c2fadc1fc1be72964ea4e12a810f7bd47
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-07-10 10:26:30 +0530

    Time: 1 ms (99.42%), Space: 42.2 MB (26.85%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,15 +1,19 @@
 class Solution {
     public int minOperations(String[] logs) {
-        int depth=0;
+      // the main conecpt is in the last step what the no of 
+        //folder present that no of folder actually we have to return 
+         
+        // let take no of folder 0
+        int count=0;
         for(String arr: logs){
             if(arr.equals("../")){
-                if(depth>0)
-                    depth--;
+                if(count>0)
+                    count--;
             }
-            else if(!arr.equals("./")){
-                depth++;
+             else if(!arr.equals("./")){
+                count++;
             }
         }
-        return depth;
+        return count;
     }
 }
