648c043a2ddb70d326d2c791564901223428012f
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-07-11 14:31:35 +0530

    Time: 1 ms (98.97%), Space: 41.5 MB (25.93%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,28 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<Character>();
-            for(int i=0;i<s.length();i++){
-                if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='[')
-                    st.push(s.charAt(i));
-                else{
-                    if(st.isEmpty())
-                        return false;
-                    else{
-                        char c=st.peek();
-                        st.pop();
-                        if((s.charAt(i)==')' && c=='(') ||(s.charAt(i)=='}' && c=='{') || (s.charAt(i)==']' && c=='['));
-                        else
-                            return false;
-                    
-                    }
-                        
-                }
-
-                
+       
+    Stack<Character> st=new Stack<>();
+        char[] arr=s.toCharArray();
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]=='(' || arr[i]=='{' || arr[i]=='['){
+                st.push(arr[i]);
             }
-        if(st.isEmpty())
-            return true;
-        else
-            return false;
-    }
+            else{
+                if(st.isEmpty())
+                    return false;
+                else{
+                    
+                    if((arr[i]==')' && st.peek()=='(') || (arr[i]=='}' && st.peek()=='{') || (arr[i]==']' && st.peek()=='[') ){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+            return st.isEmpty();
+        }
 }
