9e7b67d461b7da57bd5b92103d824ce562ede3e4
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-07-12 14:47:43 +0530

    Time: 5 ms (55.77%), Space: 43.3 MB (44.71%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,26 +1,24 @@
 class Solution {
     public String simplifyPath(String path) {
-        //we have to remove .. nd // nd . from this string 
-        // so we need String builder becoz we want to perform 
-        // some modify operation on this string
+        // string builder
         
-        // so 1stly we can split this string into the component where 
-        // single / present 
-        // then try to remove // . and .. 
-        
-        // step1 take the stack of string
-        
-        Stack<String> st=new Stack<>();
-        int n=path.length();
+    //take the array who will contain the string in between / 
         String[] p=path.split("/");
         
-        for(int i=0;i<p.length;i++){
+       Stack<String> st=new Stack<>(); 
+        
+        // traverse the each splited component
+        
+        for(int i=0;i<p.length;i++ ){
+            // .. present
+            
             if(p[i].equals("..")){
+                
                 if(!st.isEmpty()){
                     st.pop();
                 }
+                    
             }
-            
             else if(!p[i].equals("") && !p[i].equals(".")){
                 st.push(p[i]);
             }
@@ -29,16 +27,14 @@ class Solution {
         StringBuilder sb=new StringBuilder();
         
         while(!st.isEmpty()){
-            
             sb.insert(0,st.pop()).insert(0,"/");
         }
         
         if(sb.length()==0){
             return "/";
         }
-        
-        else{
+        else
             return sb.toString();
-        }
+        
     }
 }
